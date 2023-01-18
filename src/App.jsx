b441b08c3a3896876e514675a4733be4eecc5dde
omar3o3 <otantawy53@gmail.com>
2023-01-18 09:31:00 -0500

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar/>
     </div>
   )
 }
