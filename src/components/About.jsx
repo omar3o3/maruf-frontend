23c4318ae1c6b60e9cd88e6cb3450ba9c71a0047
omar3o3 <otantawy53@gmail.com>
2023-01-21 16:08:39 -0500

    font size is now responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,57 +1,79 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import {
+  createTheme,
+  ThemeProvider,
+  styled,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 
 function About() {
+
+  let theme = createTheme();
+    theme.typography.h6 = {
+      fontSize: "1.2rem",
+      "@media (min-width:600px)": {
+        fontSize: "1.5rem",
+      },
+      [theme.breakpoints.down("md")]: {
+        fontSize: "3rem",
+      },
+    };
+
   return (
-    <div id="about-page">
-      <div>
-        <Typography variant="h2" align="center" style={{ marginTop: "7%" }}>
-          About Us
-        </Typography>
-        <Typography
-          align="center"
-          style={{
-            paddingLeft: "3%",
-            paddingRight: "3%",
-            marginTop: "3%",
-            fontWeight: "bold",
-          }}
-        >
-          The Maruf community is a warm and welcoming space that brings together
-          young Muslim professionals in New York city who aspire to grow
-          spiritually and learn more about their religion in a safe, welcoming,
-          and judgment free environment. Many young Muslims find it difficult to
-          tackle their problems and by creating a community where we focus on
-          these discussions and directly address issues young Muslims are faced
-          with daily, we are able to tackle the doubts and misunderstandings
-          that may cause doubts within our community. We host community events,
-          open faith discussions, volunteering opportunities, and much more, all
-          with the hopes of bringing a diversified group of young Muslim
-          individuals together and strengthening our companionship as a
-          community.
-        </Typography>
+    <ThemeProvider theme={theme}>
+      <div id="about-page">
+        <div>
+          <Typography variant="h2" align="center" style={{ marginTop: "7%" }}>
+            About Us
+          </Typography>
+          <Typography
+            align="center"
+            variant="h6"
+            style={{
+              paddingLeft: "3%",
+              paddingRight: "3%",
+              marginTop: "3%",
+              fontWeight: "bold",
+            }}
+          >
+            The Maruf community is a warm and welcoming space that brings
+            together young Muslim professionals in New York city who aspire to
+            grow spiritually and learn more about their religion in a safe,
+            welcoming, and judgment free environment. Many young Muslims find it
+            difficult to tackle their problems and by creating a community where
+            we focus on these discussions and directly address issues young
+            Muslims are faced with daily, we are able to tackle the doubts and
+            misunderstandings that may cause doubts within our community. We
+            host community events, open faith discussions, volunteering
+            opportunities, and much more, all with the hopes of bringing a
+            diversified group of young Muslim individuals together and
+            strengthening our companionship as a community.
+          </Typography>
+        </div>
+        <div>
+          <Typography variant="h2" align="center" style={{ marginTop: "7%" }}>
+            Open Faith Discussions
+          </Typography>
+          <Typography
+            align="center"
+            variant="h6"
+            style={{
+              paddingLeft: "3%",
+              paddingRight: "3%",
+              marginTop: "3%",
+              // marginBottom: "10%",
+              paddingBottom: "10%",
+              fontWeight: "bold",
+            }}
+          >
+            A space where young professionals can learn to spiritually grow and
+            establish genuine companionship within a socially accepting
+            environment.
+          </Typography>
+        </div>
       </div>
-      <div>
-        <Typography variant="h4" align="center" style={{ marginTop: "7%" }}>
-          Open Faith Discussions
-        </Typography>
-        <Typography
-          align="center"
-          style={{
-            paddingLeft: "3%",
-            paddingRight: "3%",
-            marginTop: "3%",
-            // marginBottom: "10%",
-            paddingBottom: "10%",
-            fontWeight: "bold",
-          }}
-        >
-          A space where young professionals can learn to spiritually grow and
-          establish genuine companionship within a socially accepting
-          environment.
-        </Typography>
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,111 +2,61 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import bgImage from "../images/main-bg.jpg";
 import bgImage2 from "../images/bg-pic.png";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import {
+  createTheme,
+  ThemeProvider,
+  styled,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 
 function LandingPage() {
 
-const breakpoints = {
-  values: {
-    xs: 0,
-    sm: 0, // Phone
-    md: 768, // Tablet/Laptop
-    lg: 1500, // Desktop
-    xl: 2000,
-  },
-};
+  let theme = createTheme();
 
-const theme = createTheme({
-  breakpoints,
-  typography: {
-    h5: {
-      fontSize: "1.5625rem",
-      [`@media screen and (max-width: ${breakpoints.values.lg}px)`]: {
-        fontSize: "0.6785rem",
-      },
+  theme.typography.h6 = {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
     },
-  },
-});
+    [theme.breakpoints.down("md")]: {
+      fontSize: "3rem",
+    },
+  };
 
 
   return (
-    // <ThemeProvider theme={theme}>
-    <div id="landing-page" className="container-css">
-      <div style={{ padding: "5%" }}>
-        <Typography
-          variant="h1"
-        //   style={{ marginTop: "2%" }}
-          className="text-content"
-        >
-          Maruf Community
-        </Typography>
-        <Typography
-          variant="h6"
-          style={{ marginTop: "5%", fontWeight: "bold" }}
-          className="text-content"
-        >
-          We chose the word "Maruf" to be the name of our community because at
-          the core it means something that is well known to be good, which is
-          what we strive for. The prophetic teachings, healthy social and
-          learning environment, and most importantly the genuine companionship
-          developed and experienced at Marut, all goes back to our goal which is
-          to create a space that is inclusive of all, enjoining and encouraging
-          the good.
-        </Typography>
-        <Typography variant="h6" style={{marginTop: "10%"}}>Every Tuesday at 7:30</Typography>
+    <ThemeProvider theme={theme}>
+      <div
+        id="landing-page"
+        className="container-css"
+      >
+        <div style={{ padding: "5%" }}>
+            <Typography
+              variant="h1"
+              //   style={{ marginTop: "2%" }}
+              className="text-content"
+            >
+              Maruf Community
+            </Typography>
+            <Typography
+              variant="h6"
+              style={{ marginTop: "5%", fontWeight: "bold" }}
+              className="text-content"
+            >
+              We chose the word "Maruf" to be the name of our community because
+              at the core it means something that is well known to be good,
+              which is what we strive for. The prophetic teachings, healthy
+              social and learning environment, and most importantly the genuine
+              companionship developed and experienced at Marut, all goes back to
+              our goal which is to create a space that is inclusive of all,
+              enjoining and encouraging the good.
+            </Typography>
+          <Typography variant="h6" style={{ marginTop: "10%" }}>
+            Every Tuesday at 7:30
+          </Typography>
+        </div>
       </div>
-    </div>
-    // </ThemeProvider>
-
-    // <div className="container">
-    //   <Typography variant="h1" className="text-on-image">
-    //     Maruf Community
-    //   </Typography>
-    //   {/* <div
-    //     className="bg-image"
-    //     style={{
-    //       backgroundImage: { bgImage },
-    //       opacity: 0.5,
-    //     }}
-    //   /> */}
-    //   <img src={bgImage} alt="" style={{ width: "100%", opacity: ".6" }} />
-    //   <div className="text-content">
-    //     <p>
-    //       This is the text content that should not be affected by the opacity of
-    //       the background image
-    //     </p>
-    //   </div>
-    // </div>
-
-    // <div className="container" style={{ position: "relative" }}>
-    //   <img
-    //     src={bgImage}
-    //     alt="background"
-    //     style={{ opacity: 0.6, width: "100%" }}
-    //   />
-    //   <div
-    //     className="text-content"
-    //     style={{
-    //       position: "absolute",
-    //       color: "black",
-    //       top: "50%",
-    //       left: "50%",
-    //       transform: "translate(-50%, -50%)",
-    //     }}
-    //   >
-    //     {/* <p>This is the text content that should be displayed over the image</p> */}
-    //     <Typography variant="h1">Maruf Community</Typography>
-    //     <Typography varient="body1">
-    //       We chose the word "Maruf" to be the name of our community because at
-    //       the core it means something that is well known to be good, which is
-    //       what we strive for. The prophetic teachings, healthy social and
-    //       learning environment, and most importantly the genuine companionship
-    //       developed and experienced at Marut, all goes back to our goal which is
-    //       to create a space that is inclusive of all, enjoining and encouraging
-    //       the good.
-    //     </Typography>
-    //   </div>
-    // </div>
+    </ThemeProvider>
   );
 }
 
